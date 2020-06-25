a123b03edf9868027959a78c4abdfc42e2ce1e1d
Yank1n <59924662+Yank1n@users.noreply.github.com>
2020-06-25 22:36:20 +0500

    Add project

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 pos = new Vector3(0, 6, -9);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + pos;
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 16f;
+    float turnSpeed = 65f;
+    float forwardInput = 0f;
+    float horizontalInput = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * turnSpeed);
+    }
+}
